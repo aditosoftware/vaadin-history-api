15372acc71a44793ca3e3838e54a73850d2297cf
Andre Loreth <andre@loreth.co>
2019-11-30 12:38:34 +0100

    chore(HistoryAPIServerRpc): add comments

--- history-api-addon/src/main/java/de/aditosoftware/vaadin/addon/client/rpc/HistoryAPIServerRpc.java
+++ history-api-addon/src/main/java/de/aditosoftware/vaadin/addon/client/rpc/HistoryAPIServerRpc.java
@@ -3,8 +3,15 @@ package de.aditosoftware.vaadin.addon.client.rpc;
 import com.vaadin.shared.communication.ServerRpc;
 import de.aditosoftware.vaadin.addon.client.event.PopStateEvent;
 
-@FunctionalInterface
+/**
+ * Describes the client-to-server communication.
+ */
 public interface HistoryAPIServerRpc extends ServerRpc {
+    /**
+     * Will trigger a PopState event on the server-side using the given PopStateEvent.
+     *
+     * @param pPopStateEvent The event payload.
+     */
     void onPopState(PopStateEvent pPopStateEvent);
 }
 
