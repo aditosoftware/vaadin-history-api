3d51607c0cf1ce211bb8b53cc5b71e1d5a5237d8
Andre Loreth <andre.loreth@live.de>
2019-11-30 15:13:51 +0100

    chore(PopStateEvent): add comments

--- history-api-addon/src/main/java/de/aditosoftware/vaadin/addon/event/PopStateEvent.java
+++ history-api-addon/src/main/java/de/aditosoftware/vaadin/addon/event/PopStateEvent.java
@@ -1,29 +1,54 @@
 package de.aditosoftware.vaadin.addon.event;
 
+import de.aditosoftware.vaadin.addon.HistoryAPI;
+import java.util.EventObject;
 import java.util.Map;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Represents an PopState event.
  */
-public class PopStateEvent {
+public class PopStateEvent extends EventObject {
 
   private String uri;
   private String state;
   private Map<String, String> stateMap;
 
-  public PopStateEvent(String uri, String state, Map<String, String> pStateMap) {
+  public PopStateEvent(HistoryAPI pSource, String uri, String state,
+      Map<String, String> pStateMap) {
+    super(pSource);
     this.uri = uri;
     this.state = state;
   }
 
-  public String getUrl() {
+  /**
+   * Will return the current URI.
+   *
+   * @return The current URI.
+   */
+  @NotNull
+  public String getUri() {
     return uri;
   }
 
+  /**
+   * Will return the state of the current history as string.
+   *
+   * @return The state of the history.
+   */
+  @Nullable
   public String getState() {
     return state;
   }
 
+  /**
+   * Will return the state of the current history decoded into an string/string map. This method can
+   * be null even if {@link this#getState()} is not null.
+   *
+   * @return The state of the history decoded as string/string map.
+   */
+  @Nullable
   public Map<String, String> getStateAsMap() {
     return stateMap;
   }
