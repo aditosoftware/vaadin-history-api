c96415374daabd3057d2c2989b0a75a1f783bb34
Andre Loreth <andre.loreth@live.de>
2019-12-01 12:46:32 +0100

    chore: update demo

--- history-api-demo/src/main/java/de/aditosoftware/vaadin/addon/historyapi/demo/DemoUI.java
+++ history-api-demo/src/main/java/de/aditosoftware/vaadin/addon/historyapi/demo/DemoUI.java
@@ -1,19 +1,23 @@
 package de.aditosoftware.vaadin.addon.historyapi.demo;
 
+import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import de.aditosoftware.vaadin.addon.historyapi.HistoryAPI;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.servlet.annotation.WebServlet;
 
 @Theme("demo")
 @Title("MyComponent Add-on Demo")
+@Push()
 @SuppressWarnings("serial")
 public class DemoUI extends UI {
 
@@ -25,20 +29,38 @@ public class DemoUI extends UI {
 
   @Override
   protected void init(VaadinRequest request) {
+    HistoryAPI historyAPI = HistoryAPI.forUI(UI.getCurrent());
+
     // Show it in the middle of the screen
     final VerticalLayout layout = new VerticalLayout();
-    layout.setStyleName("demoContentLayout");
-    layout.setSizeFull();
+    layout.setWidth(25, Unit.PERCENTAGE);
     layout.setMargin(false);
     layout.setSpacing(false);
     setContent(layout);
 
-    HistoryAPI historyAPI = HistoryAPI.forUI(UI.getCurrent());
+    historyAPI.addPopStateListener(event -> {
+      Notification.show("History changed", event.getUri().toString(), Type.HUMANIZED_MESSAGE);
+    });
 
-    historyAPI.addPopStateListener(popStateEvent -> System.out.println(popStateEvent));
+    AtomicInteger counter = new AtomicInteger(1);
 
-    Map<String, String> state = new HashMap<>();
-    state.put("id", "test");
-    historyAPI.pushState(state, "", "/test");
+    Button pushButton = new Button("Push state");
+    pushButton.addClickListener(
+        event -> historyAPI.pushState((String) null, null, "/push/" + counter.getAndIncrement()));
+    layout.addComponent(pushButton);
+
+    Button replaceButton = new Button("Replace state");
+    replaceButton.addClickListener(event -> historyAPI
+        .replaceState((String) null, null, "/replace/" + counter.getAndIncrement()));
+    layout.addComponent(replaceButton);
+
+    Button backButton = new Button("Back");
+    backButton.addClickListener(event -> historyAPI.back());
+    layout.addComponent(backButton);
+
+    Button forwardButton = new Button("Forward");
+    forwardButton.addClickListener(event -> historyAPI.forward());
+    layout.addComponent(forwardButton);
+
   }
 }
