d6ba363ed0de7610d02377660b4acbff22e08976
Andre Loreth <a.loreth@adito.de>
2020-02-26 08:34:58 +0100

    feat: use wrapper widget for renderer

--- history-api-addon/src/main/java/de/aditosoftware/vaadin/addon/historyapi/client/renderer/HistoryLinkRenderer.java
+++ history-api-addon/src/main/java/de/aditosoftware/vaadin/addon/historyapi/client/renderer/HistoryLinkRenderer.java
@@ -2,44 +2,44 @@ package de.aditosoftware.vaadin.addon.historyapi.client.renderer;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.user.client.ui.Anchor;
-import com.vaadin.client.renderers.Renderer;
-import com.vaadin.client.renderers.TextRenderer;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.vaadin.client.renderers.WidgetRenderer;
 import com.vaadin.client.widget.grid.RendererCellReference;
 import de.aditosoftware.vaadin.addon.historyapi.client.util.HistoryLinkUtil;
 import elemental.json.JsonObject;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
 
-public class HistoryLinkRenderer extends WidgetRenderer<JsonObject, Anchor> {
+public class HistoryLinkRenderer extends WidgetRenderer<JsonObject, SimplePanel> {
+  private static final String WRAPPER_STYLE_NAME = "v-history-link-renderer-wrapper";
   private static final String PRIMARY_STYLE_NAME = "v-history-link-renderer";
   private static final String DATA_URI = "uri";
   private static final String DATA_TEXT = "text";
-  private final Renderer<String> fallbackRenderer;
   private final Consumer<String> clickCallback;
 
   public HistoryLinkRenderer (Consumer<String> clickCallback) {
     this.clickCallback = clickCallback;
-    fallbackRenderer = createFallbackRenderer();
   }
 
   @Override
-  public Anchor createWidget () {
-    Anchor anchor = new Anchor();
-    anchor.setStylePrimaryName(PRIMARY_STYLE_NAME);
-    anchor.addClickHandler(event -> handleAnchorClick(anchor.getHref(), event));
-    return anchor;
+  public SimplePanel createWidget () {
+    SimplePanel panel = new SimplePanel();
+    panel.setStylePrimaryName(WRAPPER_STYLE_NAME);
+    return panel;
   }
 
   @Override
-  public void render (RendererCellReference cell, JsonObject data, Anchor widget) {
-    if (!data.hasKey("uri") || !data.hasKey("text")) {
-      widget.setHref((String) null);
-      widget.setText(null);
-    } else {
-      widget.setHref(data.get("uri").asString());
-      widget.setText(data.get("text").asString());
+  public void render (RendererCellReference cell, JsonObject data, SimplePanel widget) {
+    if (!data.hasKey(DATA_URI) && data.hasKey(DATA_TEXT)) {
+      widget.setWidget(null);
+      // If there is no URI, but TEXT.
+      widget.getElement().setInnerText(data.get(DATA_TEXT).asString());
+    } else if (data.hasKey(DATA_URI) && data.hasKey(DATA_TEXT)) {
+      Anchor anchor = new Anchor(data.get(DATA_TEXT).asString(), data.get(DATA_URI).asString());
+      anchor.setStylePrimaryName(PRIMARY_STYLE_NAME);
+      anchor.addClickHandler(event -> handleAnchorClick(anchor.getHref(), event));
+
+      widget.setWidget(anchor);
     }
   }
 
@@ -52,16 +52,5 @@ public class HistoryLinkRenderer extends WidgetRenderer<JsonObject, Anchor> {
   private void handleAnchorClick (String uri, ClickEvent event) {
     if (HistoryLinkUtil.handleAnchorClick(uri, event))
       clickCallback.accept(uri);
-  }
-
-  /**
-   * Will create a TextRenderer, which can be used as fallback to represent
-   * just text.
-   *
-   * @return The created renderer.s
-   */
-  @NotNull
-  private Renderer<String> createFallbackRenderer () {
-    return new TextRenderer();
   }
 }
