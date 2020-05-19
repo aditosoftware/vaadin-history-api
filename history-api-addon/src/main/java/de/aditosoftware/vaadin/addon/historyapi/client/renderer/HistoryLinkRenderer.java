82b13696b5060e9ed37711703d6b71d537b810d0
Andre Loreth <a.loreth@adito.de>
2020-05-19 08:58:04 +0200

    feat: incrementally update the widget

--- history-api-addon/src/main/java/de/aditosoftware/vaadin/addon/historyapi/client/renderer/HistoryLinkRenderer.java
+++ history-api-addon/src/main/java/de/aditosoftware/vaadin/addon/historyapi/client/renderer/HistoryLinkRenderer.java
@@ -3,11 +3,13 @@ package de.aditosoftware.vaadin.addon.historyapi.client.renderer;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.vaadin.client.renderers.WidgetRenderer;
 import com.vaadin.client.widget.grid.RendererCellReference;
 import de.aditosoftware.vaadin.addon.historyapi.client.util.HistoryLinkUtil;
 import elemental.json.JsonObject;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class HistoryLinkRenderer extends WidgetRenderer<JsonObject, SimplePanel> {
@@ -31,15 +33,49 @@ public class HistoryLinkRenderer extends WidgetRenderer<JsonObject, SimplePanel>
   @Override
   public void render (RendererCellReference cell, JsonObject data, SimplePanel widget) {
     if (!data.hasKey(DATA_URI) && data.hasKey(DATA_TEXT)) {
-      widget.setWidget(null);
-      // If there is no URI, but TEXT.
-      widget.getElement().setInnerText(data.get(DATA_TEXT).asString());
-    } else if (data.hasKey(DATA_URI) && data.hasKey(DATA_TEXT)) {
-      Anchor anchor = new Anchor(data.get(DATA_TEXT).asString(), data.get(DATA_URI).asString());
-      anchor.setStylePrimaryName(PRIMARY_STYLE_NAME);
-      anchor.addClickHandler(event -> handleAnchorClick(anchor.getHref(), event));
+      // There is only text but no link. Clear widget and set inner text.
 
-      widget.setWidget(anchor);
+      // Load the current data of the text.
+      String dataText = data.get(DATA_TEXT).asString();
+
+      // Clear the current widget if there is any.
+      if (widget.getWidget() != null) {
+        widget.setWidget(null);
+      }
+
+      // Update the inner text of the widget element.
+      widget.getElement().setInnerText(dataText);
+    } else if (data.hasKey(DATA_URI) && data.hasKey(DATA_TEXT)) {
+      // There is text and a link. Create or incrementally update the widget.
+
+      // Load the data.
+      String dataText = data.get(DATA_TEXT).asString();
+      String dataURI = data.get(DATA_URI).asString();
+
+      // Load the existing widget if it exists.
+      Widget existing = widget.getWidget();
+      if (existing instanceof Anchor) {
+        // There is an existing Anchor Widget, therefore we can incrementally
+        // update the widget.
+        Anchor anchor = (Anchor) existing;
+
+        // Check for equality and update the data if needed.
+        if (!Objects.equals(dataText, anchor.getText()))
+          anchor.setText(data.get(DATA_TEXT).asString());
+
+        if (!Objects.equals(dataURI, anchor.getHref()))
+          anchor.setHref(data.get(DATA_URI).asString());
+      } else {
+        // Either there is no widget or the current widget is no Anchor,
+        // therefore create a new Anchor Widget, initialize it and set it as widget.
+        Anchor anchor = new Anchor(dataText, dataURI);
+        anchor.setStylePrimaryName(PRIMARY_STYLE_NAME);
+        anchor.addClickHandler(event -> handleAnchorClick(anchor.getHref(), event));
+        widget.setWidget(anchor);
+      }
+    } else {
+      // What?
+      widget.setWidget(null);
     }
   }
 
