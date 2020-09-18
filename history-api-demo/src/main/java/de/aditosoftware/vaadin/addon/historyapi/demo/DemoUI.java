8c9ac93e1f5683d2207baca3b9912cacba79af51
Andre Loreth <a.loreth@adito.de>
2020-09-18 08:11:46 +0200

    chore(demo): add id attributes for test cases

--- history-api-demo/src/main/java/de/aditosoftware/vaadin/addon/historyapi/demo/DemoUI.java
+++ history-api-demo/src/main/java/de/aditosoftware/vaadin/addon/historyapi/demo/DemoUI.java
@@ -1,40 +1,36 @@
 package de.aditosoftware.vaadin.addon.historyapi.demo;
 
 import com.sun.tools.javac.util.List;
-import com.vaadin.annotations.Push;
-import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.Title;
-import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.*;
 import com.vaadin.icons.VaadinIcons;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
+import com.vaadin.server.*;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.renderers.TextRenderer;
-import de.aditosoftware.vaadin.addon.historyapi.HistoryAPI;
-import de.aditosoftware.vaadin.addon.historyapi.HistoryLink;
-import de.aditosoftware.vaadin.addon.historyapi.HistoryLinkRenderer;
+import de.aditosoftware.vaadin.addon.historyapi.*;
 
 import javax.servlet.annotation.WebServlet;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Theme("demo")
-@Title("MyComponent Add-on Demo")
+@Title("History-API Demo")
 @Push()
 @SuppressWarnings("serial")
-public class DemoUI extends UI {
+public class DemoUI extends UI
+{
 
   @WebServlet(value = "/*", asyncSupported = true)
   @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
-  public static class Servlet extends VaadinServlet {
+  public static class Servlet extends VaadinServlet
+  {
 
   }
 
   @Override
-  protected void init (VaadinRequest request) {
+  protected void init(VaadinRequest request)
+  {
     HistoryAPI historyAPI = HistoryAPI.forUI(UI.getCurrent());
 
 
@@ -64,24 +60,29 @@ public class DemoUI extends UI {
     AtomicInteger counter = new AtomicInteger(1);
 
     Button pushButton = new Button("Push state");
+    pushButton.setId("test-mod-button-push-1");
     pushButton.addClickListener(
         event -> historyAPI.pushState("/push/" + counter.getAndIncrement()));
     buttonLayout.addComponent(pushButton);
 
     Button replaceButton = new Button("Replace state");
+    replaceButton.setId("test-mod-button-replace-1");
     replaceButton.addClickListener(event -> historyAPI
         .replaceState("/replace/" + counter.getAndIncrement()));
     buttonLayout.addComponent(replaceButton);
 
     Button backButton = new Button("Back");
+    backButton.setId("test-mod-button-back-1");
     backButton.addClickListener(event -> historyAPI.back());
     buttonLayout.addComponent(backButton);
 
     Button forwardButton = new Button("Forward");
+    forwardButton.setId("test-mod-button-forward-1");
     forwardButton.addClickListener(event -> historyAPI.forward());
     buttonLayout.addComponent(forwardButton);
 
     Button pushWithStateButton = new Button("Push state (with state object)");
+    pushWithStateButton.setId("test-mod-button-push-state-1");
     pushWithStateButton.addClickListener(event -> {
       int incremented = counter.getAndIncrement();
 
@@ -93,17 +94,21 @@ public class DemoUI extends UI {
     buttonLayout.addComponent(pushWithStateButton);
 
     HistoryLink linkWrapper = new HistoryLink("TEST!", URI.create("/client"), historyAPI);
+    linkWrapper.setId("test-history-link-1");
     linkWrapper.setIcon(VaadinIcons.ANCHOR);
 
     Button focusButton = new Button("Focus link above");
     focusButton.addClickListener(event -> linkWrapper.focus());
+    focusButton.setId("test-focus-button-1");
 
     mainLayout.addComponents(linkWrapper, focusButton);
     mainLayout.addComponent(createTestGrid(historyAPI));
   }
 
-  private Grid<TestGridData> createTestGrid (HistoryAPI historyAPI) {
+  private Grid<TestGridData> createTestGrid(HistoryAPI historyAPI)
+  {
     Grid<TestGridData> grid = new Grid<>();
+    grid.setId("test-grid-1");
 
     HistoryLinkRenderer linkRenderer = new HistoryLinkRenderer(historyAPI);
 
@@ -120,20 +125,24 @@ public class DemoUI extends UI {
     return grid;
   }
 
-  private class TestGridData {
+  private class TestGridData
+  {
     public String text;
     public HistoryLinkRenderer.Data linkData;
 
-    public TestGridData (String text, HistoryLinkRenderer.Data linkData) {
+    public TestGridData(String text, HistoryLinkRenderer.Data linkData)
+    {
       this.text = text;
       this.linkData = linkData;
     }
 
-    public String getText () {
+    public String getText()
+    {
       return text;
     }
 
-    public HistoryLinkRenderer.Data getLinkData () {
+    public HistoryLinkRenderer.Data getLinkData()
+    {
       return linkData;
     }
   }
